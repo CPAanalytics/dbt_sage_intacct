9c8a1a91e4c32ba393da3f03e86966ac586add22
jchevalier <jmc083@gmail.com>
2023-12-09 14:09:53 -0600

    add departmentid and locationid

--- models/sage_intacct__profit_and_loss.sql
+++ models/sage_intacct__profit_and_loss.sql
@@ -15,6 +15,8 @@ final as (
         classification,
         currency,
         entry_state,
+        departmentid,
+        locationid
         round(cast(period_net_amount as {{ dbt.type_numeric() }}),2) as amount
     from general_ledger_by_period
 )
